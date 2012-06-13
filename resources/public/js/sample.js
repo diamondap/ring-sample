5f06ddf1262a516e6dcae5754e3954c7de89ceda
A. Diamond <diamond@sheprador.com>
2012-06-13 07:03:07 -0400

    A little UI work.

---
+++ resources/public/js/sample.js
@@ -0,0 +1,41 @@
+// Function render adapted from Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed        
+
+function App () {
+	this.dataSource = '';
+	this.cache = {};
+	this.render = function tmpl(template, data) {
+		var fn = this.cache[template] ||
+		new Function("obj",
+					  "var p=[],print=function()"+
+					  "{p.push.apply(p,arguments);};" +
+					  "with(obj)     var data=arguments[0],"+
+					  "messages=arguments[1];{p.push('" +
+					 template
+					 .replace(/[\r\t\n]/g, " ")
+					 .split("<%").join("\t")
+					 .replace(/((^|%>)[^\t]*)'/g, "$1\r") //'
+							  .replace(/\t=(.*?)%>/g, "',$1,'")
+							  .split("\t").join("');")
+							  .split("%>").join("p.push('")
+							  .split("\r").join("\\'")
+							  + "');}return p.join('');");
+					 if(!this.cache[template]) { this.cache[template] = fn; }
+					 return data ? fn( data ) : fn;
+					 };
+
+		// container is css selector for container
+		this.render_in = function(container, template, data) {
+			$(container).html(this.render(template, data));
+		};
+}
+$app = new App();
+
+function doSearch (url)
+{
+	$app.dataSource = url;
+	var gridTemplate = $("#grid-template").html();
+	$.ajaxSetup({ cache: false, dataType: 'json' });
+	$.get(url,		  
+		  function(data) { $app.render_in('#results', gridTemplate, data); });
+}
